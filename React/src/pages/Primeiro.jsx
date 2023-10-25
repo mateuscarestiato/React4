2db6ca3b9a66f12902509562a682366ce1cbb6cb
Mateus Padilha <103007201+padirex@users.noreply.github.com>
2023-10-25 16:56:40 -0300

    Add files via upload

---
+++ React/src/pages/Primeiro.jsx
@@ -0,0 +1,29 @@
+import LinkGenerico from "../components/LinkGenerico/LinkGenerico";
+import LinkSegundo from "../components/LinkSegundo";
+
+
+const Primeiro = () => {
+    const dados = [
+        ["https://www.ibmec.br/", "ibmec"],
+
+        ["https://estude.ibmec.br/ibmec-hubs", "ibmec-hubs"],
+
+        ["https://botafogo.com.br/", "botafogo"]
+    ];
+
+    const links = dados.map( (ele, i) => (
+        <LinkGenerico
+            key={i} 
+            texto={ele[1]}
+            endereco={ele[0]}
+        />
+    ))
+
+ return (<div>
+            <h5>mini título</h5>
+            <p>Meu texto</p>
+            {links}
+        </div>);
+};
+
+export default Primeiro;
